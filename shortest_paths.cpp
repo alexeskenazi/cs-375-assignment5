74013aaf47f40a1d50ade38fb685323387b2fcef
Alex Eskenazi <aeskena1@binghamton.edu>
2025-10-31 11:36:00 -0400

    starting shortest_paths

---
+++ shortest_paths.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    cout << "Shortest Paths via Capital - CS375 Assignment 5" << endl;
+    cout << "Problem B.2: All-Pairs Shortest Paths via a Given Capital" << endl;
+    
+    cout << "Algorithm 1: O(n log n) - visits allowed" << endl;
+    cout << "Algorithm 2: O(n^2) - no revisits" << endl;
+    
+    return 0;
+}
