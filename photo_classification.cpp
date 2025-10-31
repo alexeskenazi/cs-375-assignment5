7a36fe8b583a3becff3c5548fa67157b7ff1d04c
Alex Eskenazi <aeskena1@binghamton.edu>
2025-10-31 10:45:08 -0400

    initial setup cont

---
+++ photo_classification.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cout << "Photo Classification Program - CS375 Assignment 5" << endl;
+    cout << "Problem B.1: Object Classification via Disjoint Sets" << endl;
+    
+    return 0;
+}
